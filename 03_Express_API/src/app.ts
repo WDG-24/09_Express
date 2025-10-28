f740f452b23d235c947f314ad645d434ed7b00a8
onureredo <onureredo@yahoo.com>
2025-10-28 16:18:59 +0100

    api

---
+++ 03_Express_API/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import '#db';
+import { postRoutes, userRoutes } from '#routes';
+
+const app = express();
+const port = 3000;
+
+// body-parser
+app.use(express.json());
+
+// Routes
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+
+app.listen(port, () =>
+  console.log(`\x1b[35mMain app listening at http://localhost:${port}\x1b`)
+);
